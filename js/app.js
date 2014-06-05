997a2c1ecc383d098bc67e9f87f12a2711b472a3
Havmoeller <jonashavmoeller@gmail.com>
2014-06-05 21:47:30 +0200

    Front-page done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+/***************************************
+			JQUERY STUFF
+***************************************/
+ 
+// Get #content height
+var content = $("#content").height();
+
+
+// Add #content's height to sidebar + 3em
+$('#sidebar').css( "height", content + 48 );
